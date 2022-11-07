fe5ebc2737c60c9f9c5f5624fa1cce07f9617c5e
rizalshmbg <sihombingrizal18@gmail.com>
2022-11-07 21:54:39 +0700

    completed single product add to cart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 import { FaCheck } from 'react-icons/fa'
 import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
-
+/*  */
 const AddToCart = ({ product }) => {
 	const { id, stock, colors } = product
 
